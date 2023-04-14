6b65ff647717c0e77bb437cbb36aea8d2d73bfef
worthless443 <aissypewdiepie@gmail.com>
2023-04-15 04:37:01 +0530

    fclose() is causing double free()

--- getcmd.c
+++ getcmd.c
@@ -5,7 +5,7 @@
 
 #include<batparse.h>
 
-static int getStdOutInput(const char *command, char *buffer) {
+static FILE *getStdOutInput(const char *command, char *buffer) {
 	int ret = 0,i;
 	char buf[1];
 	FILE *fp = popen(command, "r");
@@ -13,17 +13,17 @@ static int getStdOutInput(const char *command, char *buffer) {
 		strcat(buffer,buf);
 		ret+=i;
 	}
-	fclose(fp);
-	return ret;
+	return fp;
 }
 int bat_parse(struct BatSt *st) {
     char *buffer = malloc(sizeof(char)*2000);
 	char buf[1000];
 	char buf1[1000];
 	const char *command = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
-	getStdOutInput(command, buffer);
+	FILE *fp = getStdOutInput(command, buffer);
 	st->pert = parse_str(buffer, buf);
 	st->ret = parse_charge(buffer,buf1);
+	//fclose(fp);
 	free(buffer);
 	return 1;
 }
