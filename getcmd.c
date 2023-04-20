3cd904a155e402ceb4747afdd7afaead77783e4b
worthless443 <aissypewdiepie@gmail.com>
2023-04-21 01:48:31 +0530

    not needed

--- getcmd.c
+++ getcmd.c
@@ -41,14 +41,14 @@ int only_process_wrapper(int offset) {
 	return 1;
 }
 
-char *only_process_wrapper_str(int offset) {
-	char *cmd = stdout_getcmd_constant("ps -a",10000);
-	int nl_size = calc_newline(cmd);
-	char **lines = split_ps_buffer(cmd);
-	char **procs = get_processes(lines,nl_size);	
-	char *proc_str = store_process_string(procs,nl_size,offset);
-	split_buffer_free(lines,nl_size );
-	proc_free(procs,nl_size);
-	free(cmd);
-	return proc_str;
-}
+//char *only_process_wrapper_str(int offset) {
+//	char *cmd = stdout_getcmd_constant("ps -a",10000);
+//	int nl_size = calc_newline(cmd);
+//	char **lines = split_ps_buffer(cmd);
+//	char **procs = get_processes(lines,nl_size);	
+//	char *proc_str = store_process_string(procs,nl_size,offset);
+//	split_buffer_free(lines,nl_size );
+//	proc_free(procs,nl_size);
+//	free(cmd);
+//	return proc_str;
+//}
